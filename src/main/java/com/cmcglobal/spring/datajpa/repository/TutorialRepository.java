a5facca05d2da2e221e4ec1999a8e988f0e1cf1a
Nguyễn Hữu Cảnh - CMC Global DU3 <nhcanh@ldap.cmcglobal.com.vn>
2021-10-13 13:40:43 +0700

    init project demo

---
+++ src/main/java/com/cmcglobal/spring/datajpa/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.cmcglobal.spring.datajpa.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cmcglobal.spring.datajpa.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+	List<Tutorial> findByPublished(boolean published);
+	List<Tutorial> findByTitleContaining(String title);
+}
